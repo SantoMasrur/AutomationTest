e76c6c50f7e95ce3512c875f8dfd2e4ad9b9e21b
SantoMasrur <masrurbd67@gmail.com>
2023-04-07 23:43:23 +0600

    Home Assignment

--- src/NewTest.java
+++ src/NewTest.java
@@ -36,15 +36,37 @@ public class NewTest {
         driver.get("https://chaldal.com/");
         Thread.sleep(5000);//waits 5 seconds after first click
         driver.manage().window().maximize();//browser maximize{full screen}
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
         //driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/h5")).click();
         //Thread.sleep(5000);
         //driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div[3]/h5")).click();
         //Thread.sleep(5000);
         //driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/ul[1]/li[1]/div/a/span[1]")).click();
 
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys("Egg");
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys(Keys.ENTER);
+        //driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys("Egg");
+        //driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys(Keys.ENTER);
+
+        //////Home Work/////
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys("Egg");//search for egg :)
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div/div[1]/div[1]/form/div/div[1]/input")).sendKeys(Keys.ENTER);//hit enter
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[6]/section/div/div/div/div/section/div[3]/div[2]/div[1]/div/section/p")).click();//add to carts
+        Thread.sleep(2000);
+
+        for(int i = 0; i < 4; i++ ) {
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[6]/section/div/div/div/div/section/div[3]/div[2]/div[1]/div/div[2]/button[2]")).click();//click "+" 5 times
+            Thread.sleep(2000);
+        }
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/section/div[1]/p/span")).click();//checkout
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/div/button/span[1]")).click();//place order
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[5]/section/div/div/form/div[1]/div/div[1]/input")).sendKeys("01738912074");//add phone number
+
+
 
 
     }
